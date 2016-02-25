3aa2a59c772c43948f324756f7e355a69ac5c74d
Urednick-Brandon <ure09002@byui.edu>
2016-02-25 14:34:59 -0500

    Update HTTPreader.java

--- HTTPreader/src/httpreader/HTTPreader.java
+++ HTTPreader/src/httpreader/HTTPreader.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package httpreader;
 
-/**
- *
- * @author Brandon Urednick
- */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
