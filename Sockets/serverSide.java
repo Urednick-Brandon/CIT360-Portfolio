e8c51ca623e5152636e19d247456c852fe726e94
Urednick-Brandon <ure09002@byui.edu>
2016-06-25 21:59:05 -0400

    Update serverSide.java

--- Sockets/serverSide.java
+++ Sockets/serverSide.java
@@ -1,1 +1,37 @@
 
+import java.net.*;
+import java.io.*;
+ 
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+         
+        if (args.length != 1) {
+            System.err.println("Usage: java EchoServer <port number>");
+            System.exit(1);
+        }
+         
+        int portNumber = Integer.parseInt(args[0]);
+         
+        try (
+          
+          //created port number to listen on based on input and creates buffers for IO.
+            ServerSocket serverSocket =
+                new ServerSocket(Integer.parseInt(args[0]));
+            Socket clientSocket = serverSocket.accept();     
+            PrintWriter out =
+                new PrintWriter(clientSocket.getOutputStream(), true);                   
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+              //prints out the input from the client
+                out.println(inputLine);
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
+}
