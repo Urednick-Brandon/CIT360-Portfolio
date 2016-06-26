e698f1b28946152bb119d5782fa897387b88e350
Urednick-Brandon <ure09002@byui.edu>
2016-06-25 22:16:42 -0400

    Update CalculatorTest.java

--- JUnits/CalculatorTest.java
+++ JUnits/CalculatorTest.java
@@ -5,7 +5,7 @@ public class CalculatorTest {
   @Test
   public void evaluatesExpression() {
     Calculator calculator = new Calculator();
-    int sum = calculator.evaluate("1+2+3");
-    assertEquals(6, sum);
+    int sum = calculator.evaluate("1+6+3");
+    assertEquals(10, sum);
   }
 }
